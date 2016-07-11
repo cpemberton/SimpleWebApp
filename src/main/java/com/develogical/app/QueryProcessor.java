544f947f1d011ff5f7926bc0bf23313c9d621656
ChristianPemberton <ChristianPemberton@192.168.0.59>
2016-07-11 20:06:29 +0100

    Added a response to the process method.

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -8,6 +8,12 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
+        if (query.toLowerCase().contains("APE")) {
+            return "This course presents practical techniques for building software in an agile environment." + 
+            		"We aim to minimise the time between the conception of an idea and its implementation in working software released to users. " +
+            		"We aim to embrace the changes thrown at us by a continually changing market, and to mitigate the associated risks by using technical practices to ensure quality " +
+            		"- of our software and of the processes that we use to develop it.";
+        }
         return "";
     }
 }
