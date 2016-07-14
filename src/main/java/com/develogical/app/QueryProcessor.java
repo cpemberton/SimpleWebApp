e09d36e3478cc5c1ca6924f9d46660e15d7266b4
ChristianPemberton <ChristianPemberton@10.31.76.93>
2016-07-14 16:47:00 +0100

    8th commit

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -3,7 +3,7 @@ package com.develogical.app;
 public class QueryProcessor {
 
     public String process(String query) {
-    	
+    	// multiplied 
     	if (query.contains("what") && query.contains("multiplied")  ) {
     		double product = 1; 
     		for (int i = 0; i<query.split(" ").length; i++) {
@@ -13,7 +13,7 @@ public class QueryProcessor {
     		}
     		return Double.toString(product); 
     	}
-    	
+    	// plus 
     	if (query.contains("what") && query.contains("plus")  ) {
     		double sum = 0; 
     		for (int i = 0; i<query.split(" ").length; i++) {
@@ -23,12 +23,42 @@ public class QueryProcessor {
     		}
     		return Double.toString(sum); 
     	}	
+    	// largest 
+    	if (query.contains("which") && query.contains("largest")  ) {
+    		double max = 0; 
+    		for (int i = 0; i<query.split(" ").length; i++) {
+    			if (isNumeric(query.split(" ")[i])) {
+    				max = (Double.parseDouble(query.split(" ")[i]) > max)? Double.parseDouble(query.split(" ")[i]): max; 
+    			}
+    		}
+    		return Double.toString(max); 
+    	}
+    		
+    	
+    	// square and a cube 
+    	if (query.contains("which") && query.contains("largest")  ) {
+    		double res = 0; 
+    		for (int i = 0; i<query.split(" ").length; i++) {
+    			if (isNumeric(query.split(" ")[i])) {
+    				double squarer = Math.sqrt(Double.parseDouble(query.split(" ")[i])); 
+    				double cuber = Math.cbrt(Double.parseDouble(query.split(" ")[i])); 
+    				if ((squarer == (int)squarer) && (cuber == (int)cuber)) {
+    					res = Double.parseDouble(query.split(" ")[i]);
+    				}
+    			}
+    		}
+    		return Double.toString(res); 
+    	}	
     	
         return "";
     }
+    
+    
     
     public static boolean isNumeric(String str)
     {
       return str.matches("-?\\d+(\\.\\d+)?");  //match a number with optional '-' and decimal.
     }
+    
+    
 }
