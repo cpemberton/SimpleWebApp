d9d838dc95212cfd2c5d18f7c7867de6bebae272
ChristianPemberton <ChristianPemberton@10.31.76.93>
2016-07-14 16:33:04 +0100

    7th commit

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -3,17 +3,32 @@ package com.develogical.app;
 public class QueryProcessor {
 
     public String process(String query) {
-        if (query.toLowerCase().contains("shakespeare")) {
-            return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
-                    "English poet, playwright, and actor, widely regarded as the greatest " +
-                    "writer in the English language and the world's pre-eminent dramatist.";
-        }
-        if (query.toLowerCase().contains("APE")) {
-            return "This course presents practical techniques for building software in an agile environment." + 
-            		"We aim to minimise the time between the conception of an idea and its implementation in working software released to users. " +
-            		"We aim to embrace the changes thrown at us by a continually changing market, and to mitigate the associated risks by using technical practices to ensure quality " +
-            		"- of our software and of the processes that we use to develop it.";
-        }
+    	
+    	if (query.contains("what") && query.contains("multiplied")  ) {
+    		double product = 1; 
+    		for (int i = 0; i<query.split(" ").length; i++) {
+    			if (isNumeric(query.split(" ")[i])) {
+    				product = product * Double.parseDouble(query.split(" ")[i]);
+    			}
+    		}
+    		return Double.toString(product); 
+    	}
+    	
+    	if (query.contains("what") && query.contains("plus")  ) {
+    		double sum = 0; 
+    		for (int i = 0; i<query.split(" ").length; i++) {
+    			if (isNumeric(query.split(" ")[i])) {
+    				sum = sum + Double.parseDouble(query.split(" ")[i]);
+    			}
+    		}
+    		return Double.toString(sum); 
+    	}	
+    	
         return "";
+    }
+    
+    public static boolean isNumeric(String str)
+    {
+      return str.matches("-?\\d+(\\.\\d+)?");  //match a number with optional '-' and decimal.
     }
 }
